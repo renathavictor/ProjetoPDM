03b32244b7e36f3c61ff7e2945cdc41ab95b8b0d
Renatha <renatha.nvictor@gmail.com>
2019-11-06 19:39:59 -0300

    correcao erro quando denuncia vem null

--- app/src/main/java/com/example/projetopdm/MainActivity.kt
+++ app/src/main/java/com/example/projetopdm/MainActivity.kt
@@ -87,7 +87,6 @@ class MainActivity : AppCompatActivity() {
 
         val denuncia = intent.getSerializableExtra("DENUNCIA")
         if (denuncia != null){
-            val denuncia = intent.getSerializableExtra("DENUNCIA")  as Denuncia
             this.etTitulo.text.append((denuncia as Denuncia).titulo)
             this.etInfo.text.append((denuncia as Denuncia).descricao)
             this.etLocal.text.append((denuncia as Denuncia).localizacao)
